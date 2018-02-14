5612a8391f67106e57bcbb4710930d48324661c5
Tracy Lam <tracylam@berkeley.edu>
2018-02-14 14:08:00 -0800

    finish hw1

--- foobar.rb
+++ foobar.rb
@@ -4,5 +4,9 @@ class Foobar
     # Class method, not an instance method
     # Call with `Foobar.baz`
     # Q4 CODE HERE
+    a = a.collect{|e| e.to_i}
+    a = a.select{|x| x % 2 == 0}
+    a = a.map{|e| e + 2}
+    a.reduce(:+)
   end
 end
